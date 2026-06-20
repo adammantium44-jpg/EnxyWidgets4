40c00a995a148c070fe662ec88b68c5327499f7e
adammantium44-jpg <adammantium44@gmail.com>
2026-06-21 04:41:14 +0700

    Update Plugin.cpp

--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -1,23 +1,26 @@
-#include "Config.h"
+// plugin.cpp
 #include "PCH.h"
-#include "Ui.h"
-#include "Widget.h"
+#include "Logger.h"
+#include "UIManager.h"
+#include "Config.h"
 
 static void SKSEMessageHandler(SKSE::MessagingInterface::Message* message) {
     if (message->type == SKSE::MessagingInterface::kDataLoaded) {
-        PrismaUI = static_cast<PRISMA_UI_API::IVPrismaUI1*>(
+        using namespace Enxytemp;
+        UIManager::PrismaUI = static_cast<PRISMA_UI_API::IVPrismaUI1*>(
             PRISMA_UI_API::RequestPluginAPI(PRISMA_UI_API::InterfaceVersion::V1));
-        if (PrismaUI) {
-            auto input = RE::BSInputDeviceManager::GetSingleton();
-            if (input) input->AddEventSink(InputHandler::GetSingleton());
-            InitializeUI();
+
+        if (UIManager::PrismaUI) {
+            RE::BSInputDeviceManager::GetSingleton()->AddEventSink(Config::InputHandler::GetSingleton());
+            UIManager::Initialize();
+            Logger::Info("PrismaUI Connected");
         }
     }
 }
 
 SKSEPluginLoad(const SKSE::LoadInterface* skse) {
     SKSE::Init(skse);
-    auto messaging = SKSE::GetMessagingInterface();
-    if (messaging) messaging->RegisterListener(SKSEMessageHandler);
+    Enxytemp::Logger::Setup();
+    SKSE::GetMessagingInterface()->RegisterListener(SKSEMessageHandler);
     return true;
 }
