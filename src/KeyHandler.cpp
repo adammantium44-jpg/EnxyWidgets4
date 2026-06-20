6f40d795d4932dbe490af211706294c4d50b26c0
adammantium44-jpg <adammantium44@gmail.com>
2026-06-21 04:00:31 +0700

    Create KeyHandler.cpp

---
+++ src/KeyHandler.cpp
@@ -0,0 +1,21 @@
+#include "KeyHandler.h"
+
+#include "Ui.h"
+
+InputHandler* InputHandler::GetSingleton() {
+    static InputHandler instance;
+    return &instance;
+}
+
+RE::BSEventNotifyControl InputHandler::ProcessEvent(RE::InputEvent* const* a_events,
+                                                    RE::BSTEventSource<RE::InputEvent*>*) {
+    if (!a_events) return RE::BSEventNotifyControl::kContinue;
+    for (auto event = *a_events; event; event = event->next) {
+        auto button = event->AsButtonEvent();
+        if (button && button->IsDown() && button->GetDevice() == RE::INPUT_DEVICE::kKeyboard &&
+            button->GetIDCode() == 0x41) {
+            ToggleUI();
+        }
+    }
+    return RE::BSEventNotifyControl::kContinue;
+}
